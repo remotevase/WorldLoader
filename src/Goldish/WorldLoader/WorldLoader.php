ec9b2d515b07ee7ab64a22f85b5adc7925bf5a08
remote_vase <web.articlerequests@gmail.com>
2016-06-26 22:26:34 -0400

    Better fit sqpe

--- src/Goldish/WorldLoader/WorldLoader.php
+++ src/Goldish/WorldLoader/WorldLoader.php
@@ -12,12 +12,6 @@ class WorldLoader extends PluginBase {
   
   	public function onEnable(){
 		$this->getLogger()->info(TextFormat::AQUA . "WorldLoader loaded.");
-  	}
-         
-		 public function onCommand(CommandSender $sender, Command $command, $label, array $args){
-  			$cmd = strtolower($command->getName());
-			switch($cmd){
-				case "loadworlds":
 	        	           $sender->sendMessage(TextFormat::AQUA . "[WorldLoader]" . TextFormat::YELLOW . " All worlds has been loaded.");
 	                    
 	                	    //lol i just want to simplify it
@@ -26,6 +20,5 @@ class WorldLoader extends PluginBase {
 	        			 $this->getServer()->loadLevel($level);
 	                		}
                 		break;
-			}
-		}
+}
 }
